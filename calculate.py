9cca7cd71f76d6d9a3021cb339ab09586051de89
Eshed <ashdnazg@gmail.com>
2022-03-05 18:31:29 +0200

    numbers only

--- calculate.py
+++ calculate.py
@@ -1,41 +1,46 @@
-def is_consistent(current_digits, dec_length, bin_length):
-    dec_digits_left = dec_length - len(current_digits) * 2
-    max_dec = int(current_digits + ("9" * dec_digits_left) + current_digits[::-1])
-    min_dec = int(current_digits + ("0" * dec_digits_left) + current_digits[::-1])
-    bin_digits = bin(int(current_digits[::-1]) % (1 << len(current_digits)))[2:].zfill(len(current_digits))
-    bin_digits_left = bin_length - len(current_digits) * 2
-    max_bin = int(bin_digits[::-1] + ("1" * bin_digits_left) + bin_digits, 2)
-    min_bin = int(bin_digits[::-1] + ("0" * bin_digits_left) + bin_digits, 2)
-    return min_bin <= max_dec and max_bin >= min_dec
+import time
 
-def digits_to_num(current_digits, dec_length):
-    return int(current_digits + current_digits[dec_length - len(current_digits) - 1::-1])
+start = time.time()
 
-def check_palindrome(current_digits, dec_length, bin_length):
-    bin_num = bin(digits_to_num(current_digits, dec_length))[2:]
-    return len(bin_num) == bin_length and bin_num == bin_num[::-1]
+def find_palindrom_internal(current_num, bin_num, level, digits, dec_length, bin_length, digit_cache, max_dec_cache, max_bin_cache):
+    if (level + 1) * 2 >= dec_length:
+        for digit in digits:
+            new_num = current_num + digit * digit_cache[level]
+            bin_str = bin(new_num)[2:]
+            if bin_str == bin_str[::-1] and len(bin_str) == bin_length:
+                print("%.2f: %s" % (time.time() - start, new_num))
+        return
 
-def find_palindrom_internal(current_digits, dec_length, bin_length):
-    if len(current_digits) == 0:
-        digits = range(1, 10, 2)
-    else:
-        digits = range(10)
+    max_bin_add = max_bin_cache[level]
+    max_dec_add = max_dec_cache[level]
 
     for digit in digits:
-        new_digits = current_digits + str(digit)
+        new_num = current_num + digit * digit_cache[level]
+        new_bin_num = bin_num + (((new_num >> level) & 1) << (bin_length - level - 1))
 
-        if len(new_digits) * 2 >= dec_length:
-            if check_palindrome(new_digits, dec_length, bin_length):
-                print(digits_to_num(new_digits, dec_length))
+        if new_bin_num + max_bin_add < new_num or new_num + max_dec_add < new_bin_num:
             continue
 
-        if not is_consistent(new_digits, dec_length, bin_length):
-            continue
+        find_palindrom_internal(new_num, new_bin_num, level + 1, range(10), dec_length, bin_length, digit_cache, max_dec_cache, max_bin_cache)
 
-        find_palindrom_internal(new_digits, dec_length, bin_length)
+def get_digit_cache(dec_length):
+    cache = []
+    for i in range((dec_length + 1) // 2):
+        j = dec_length - i - 1
+        cache.append(10**i)
+        if i != j:
+            cache[-1] += 10**j
+
+    return cache
+
+def get_max_cache(length, base):
+    return [base**(length - i) - base**i for i in range(1, (length + 1) // 2)]
 
 def find_palindrome(dec_length, bin_length):
-    find_palindrom_internal("", dec_length, bin_length)
+    digit_cache = get_digit_cache(dec_length)
+    max_dec_cache = get_max_cache(dec_length, 10)
+    max_bin_cache = get_max_cache(bin_length, 2)
+    find_palindrom_internal(0, 0, 0, range(1, 10, 2), dec_length, bin_length, digit_cache, max_dec_cache, max_bin_cache)
 
 def is_possible(dec_length, bin_length):
     max_dec = int("9" * dec_length)
@@ -47,7 +52,7 @@ def is_possible(dec_length, bin_length):
 def main():
     for i in range(10):
         if bin(i)[2:] == bin(i)[:1:-1]:
-            print(i)
+            print("%.2f: %s" % (time.time() - start, i))
     dec_length = 2
     bin_length = 4
 

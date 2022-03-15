58d75116e4498bbe1e4b13e37d5a0f60e25a4e78
Eshed <ashdnazg@gmail.com>
2022-03-15 06:51:41 +0200

    optimised parameters

--- calculate.py
+++ calculate.py
@@ -2,24 +2,34 @@ import time
 
 start = time.time()
 
-CACHE_SIZE = 13
+MIN_CACHE3_SIZE = 10
+MAX_CACHE3_SIZE = 25
 
-MIN_CACHE_SIZE = 10
-MAX_CACHE_SIZE = 17
+MIN_CACHE4_SIZE = 12
+MAX_CACHE4_SIZE = 23
 
 def check_msb_lookup(current_num, max_dec_add, level, dec_length, bin_length, lookup_cache):
-    if level + 3 != ((dec_length + 1) // 2):
+    if level + 4 == ((dec_length + 1) // 2):
+        cache = lookup_cache[1]
+        min_cache_size = MIN_CACHE4_SIZE
+        max_cache_size = MAX_CACHE4_SIZE
+    elif level + 3 == ((dec_length + 1) // 2):
+        cache = lookup_cache[0]
+        min_cache_size = MIN_CACHE3_SIZE
+        max_cache_size = MAX_CACHE3_SIZE
+    else:
         return True
+
     max_dec = current_num + max_dec_add
     msb_set_digits = bin_length - len(bin(max_dec ^ current_num)) + 2
-    if msb_set_digits < (level + MIN_CACHE_SIZE):
+    if msb_set_digits < (level + min_cache_size):
         return True
-    cache_digits = min(msb_set_digits - level, MAX_CACHE_SIZE)
+    cache_digits = min(msb_set_digits - level, max_cache_size)
     modulo = 1 << cache_digits
     current_digits = (current_num >> level) % modulo
     final_bin_digits = (int(bin(max_dec)[msb_set_digits + 1:1:-1],2) >> level)
     lookup_number = ((final_bin_digits + modulo) - current_digits) % modulo
-    return lookup_number in lookup_cache[cache_digits]
+    return lookup_number in cache[cache_digits]
 
 def find_palindrom_internal(current_num, bin_num, level, digits, dec_length, bin_length, digit_cache, max_dec_cache, max_bin_cache, lookup_cache):
     if (level + 1) * 2 >= dec_length:
@@ -59,9 +69,10 @@ def get_digit_cache(dec_length):
 def get_lookup_cache(digit_cache):
     if len(digit_cache) < 3:
         return
-    cache = {}
+
+    cache3 = {}
     lsb_set_bits = len(digit_cache) - 3
-    for cache_size in range(MIN_CACHE_SIZE, MAX_CACHE_SIZE + 1):
+    for cache_size in range(MIN_CACHE3_SIZE, MAX_CACHE3_SIZE + 1):
         current_cache = set()
         modulo = 1 << cache_size
         for i in range(10):
@@ -70,9 +81,26 @@ def get_lookup_cache(digit_cache):
                     digits_sum = digit_cache[-1] * i + digit_cache[-2] * j + digit_cache[-3] * k
                     current_cache.add((digits_sum >> lsb_set_bits) % modulo)
 
-        cache[cache_size] = current_cache
+        cache3[cache_size] = current_cache
 
-    return cache
+    if len(digit_cache) < 4:
+        return
+
+    cache4 = {}
+    lsb_set_bits = len(digit_cache) - 4
+    for cache_size in range(MIN_CACHE4_SIZE, MAX_CACHE4_SIZE + 1):
+        current_cache = set()
+        modulo = 1 << cache_size
+        for i in range(10):
+            for j in range(10):
+                for k in range(10):
+                    for w in range(10):
+                        digits_sum = digit_cache[-1] * i + digit_cache[-2] * j + digit_cache[-3] * k + digit_cache[-4] * w
+                        current_cache.add((digits_sum >> lsb_set_bits) % modulo)
+
+        cache4[cache_size] = current_cache
+
+    return (cache3, cache4)
 
 
 def get_max_cache(length, base):
@@ -93,9 +121,8 @@ def is_possible(dec_length, bin_length):
     return min_bin <= max_dec and max_bin >= min_dec
 
 def main():
-    # print(get_max_cache(19, 10))
+    # find_palindrome(29, 94)
     # exit(1)
-    # find_palindrome(25, 81)
     for i in range(10):
         if bin(i)[2:] == bin(i)[:1:-1]:
             print("%.2f: %s" % (time.time() - start, i))
